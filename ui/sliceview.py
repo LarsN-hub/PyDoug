cb0427b48eb4f757c4300859bf304d4166b540a8
Nelson <d.lars.nelson@gmail.com>
2025-12-18 15:32:15 -0500

    Created ui/sliceview.py and added functions to launch and close napari as well as displaying a volume in napari.

---
+++ ui/sliceview.py
@@ -0,0 +1,38 @@
+"""
+Module for rendering images
+"""
+
+# Imports
+
+import numpy as np
+import napari
+
+
+# Functions
+
+def launch_napari() -> napari.viewer:
+    
+    viewer = napari.Viewer()
+    
+    return viewer
+
+def close_napari(viewer: napari.viewer) -> None:
+    
+    viewer.close()
+    
+def view_im(im_array: np.array, viewer: napari.viewer) -> napari.layers:
+    
+    im_layer: napari.layers = viewer.add_image(im_array)
+    
+    return im_layer
+
+
+# Main
+
+def main() -> None:
+    
+    pass
+
+if __name__ == "__main__":
+    
+    main()
