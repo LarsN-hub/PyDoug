8a64d6971ef7f7362e3e38c57916076d3ae7033d
Nelson <d.lars.nelson@gmail.com>
2026-01-02 22:08:37 -0500

    Created various mask creation functions and finished cropping function in cropclip.py

--- proc/cropclip.py
+++ proc/cropclip.py
@@ -5,11 +5,22 @@ Module for cropping, clipping, and padding image dimensions
 # Imports
 
 import numpy as np
+import math
 
 from skimage import draw
 
 
 # Functions
+
+def get_in_plane_dims(im_array: np.array) -> tuple[int]:
+    
+    if len(im_array.shape) == 2:
+        
+        return (im_array.shape[0], im_array.shape[1])
+    
+    else:
+        
+        return (im_array.shape[1], im_array.shape[2])
 
 def coords_2_lists(shape_coords: np.array) -> dict[str, list]:
     
@@ -19,45 +30,158 @@ def coords_2_lists(shape_coords: np.array) -> dict[str, list]:
     
     for index, coords in enumerate(shape_coords):
         
-        rows.append(int(coords[0]))
-        cols.append(int(coords[1]))
+        if len(coords) > 2:
+            
+            rows.append(int(coords[1]))
+            cols.append(int(coords[2]))
+        
+        else:
+            
+            rows.append(int(coords[0]))
+            cols.append(int(coords[1]))
         
     return {"rows": rows, "cols": cols}
 
-def shape_mask_coords(shape_coords: np.array, shape_type: str) -> np.array:
+def rectangle_mask(mask_shape: tuple[int], coords_dict: dict[str, list], rot_angle: float = 0) -> dict[str, np.array, tuple]:
     
-    coords_dict: dict[str, list] = coords_2_lists(shape_coords)
+    mask_array: np.array = np.zeros(mask_shape, dtype = np.bool)
     
-    if shape_type == "rectangle":
+    if rot_angle:
         
-        return draw.rectangle((min(coords_dict["rows"]), min(coords_dict["cols"])), (max(coords_dict["rows"]), max(coords_dict["cols"])))
+        r_coords = np.array(coords_dict["rows"])
+        c_coords = np.array(coords_dict["cols"])
+        rr, cc = draw.polygon(r_coords, c_coords, shape = mask_shape)
+        mask_array[rr, cc] = 1
     
-    elif shape_type == "ellipse":
+    else:
         
-        pass
+        if len(coords_dict["rows"]) == 2:
+        
+            orig_coords: tuple[int] = (coords_dict["rows"][0], coords_dict["cols"][0])
+            oppo_coords: tuple[int] = (coords_dict["rows"][1], coords_dict["cols"][1])
+            
+        else:
+            
+            orig_coords: tuple[int] = (coords_dict["rows"][0], coords_dict["cols"][0])
+            oppo_coords: tuple[int] = (coords_dict["rows"][2], coords_dict["cols"][2])
+            
+        rr, cc = draw.rectangle(orig_coords, oppo_coords, shape = mask_shape)
+        mask_array[rr, cc] = 1
+        
+    return get_mask_dict(mask_array, rr, cc)
 
-def crop(im_array: np.array, shape_type: str, shape_coords: np.array, *, outside_mask_intensity: int | float = 0) -> np.array:
+def ellipse_mask(mask_shape: tuple[int], coords_dict: dict[str, list], rot_angle: float = 0) -> dict[str, np.array, tuple]:
     
-    valid_shapes: tuple[str] = ("rectangle", "ellipse", "line")
+    mask_array: np.array = np.zeros(mask_shape, dtype = np.bool)
+    orig_coords: tuple[int] = (coords_dict["rows"][0], coords_dict["cols"][0])
+    next_coords: tuple[int] = (coords_dict["rows"][1], coords_dict["cols"][1])
+    oppo_coords: tuple[int] = (coords_dict["rows"][2], coords_dict["cols"][2])
+    prev_coords: tuple[int] = (coords_dict["rows"][3], coords_dict["cols"][3])
+    r_radius: float = math.sqrt(((prev_coords[0] - orig_coords[0])**2) + ((prev_coords[1] - orig_coords[1])**2)) / 2
+    c_radius: float = math.sqrt(((next_coords[0] - orig_coords[0])**2) + ((next_coords[1] - orig_coords[1])**2)) / 2
+    r_center: float = orig_coords[0] + ((oppo_coords[0] - orig_coords[0]) / 2)
+    c_center: float = orig_coords[1] + ((oppo_coords[1] - orig_coords[1]) / 2)
+    rr, cc = draw.ellipse(r_center, c_center, r_radius, c_radius, shape = mask_shape, rotation = rot_angle)
+    mask_array[rr, cc] = 1
+        
+    return get_mask_dict(mask_array, rr, cc)
+
+def get_mask_dict(mask_array: np.array, rr: np.array, cc: np.array) -> dict[str, np.array, tuple]:
+    
+    low_coords: tuple[int] = (np.min(rr), np.min(cc))
+    high_coords: tuple[int] = (np.max(rr), np.max(cc))
+    
+    return {"mask": mask_array, "start": low_coords, "end": high_coords}
+
+def coords_2_mask(im_array: np.array, shape_coords: np.array, shape_type: str) -> dict[str, np.array, tuple]:
+    
+    valid_shapes: tuple[str] = ("rectangle", "ellipse", "polygon")
     
     if any(shape_type.find(x) != -1 for x in valid_shapes):
+    
+        mask_shape: tuple[int] = get_in_plane_dims(im_array)
+        coords_dict: dict[str, list] = coords_2_lists(shape_coords)
         
-        coords_dict: dict[str, list] = coords_2_lists(shape_coords)            
-        crop_array: np.array = im_array[:, min(coords_dict["rows"]):max(coords_dict["rows"]), min(coords_dict["cols"]):max(coords_dict["cols"])]
+        if len(coords_dict["rows"]) == 4 and shape_type != "polygon":
+
+            orig_coords: tuple[int] = (coords_dict["rows"][0], coords_dict["cols"][0])
+            next_coords: tuple[int] = (coords_dict["rows"][1], coords_dict["cols"][1])
+            rot_angle: float = -math.atan2((next_coords[0] - orig_coords[0]), (next_coords[1] - orig_coords[1]))
         
-        if len(set(coords_dict["rows"])) > 2:
+        elif len(coords_dict["rows"]) == 2 and shape_type == "rectangle":
             
-            pass
+            rot_angle: float = 0
+            
+        else:
+            
+            r_coords = np.array(coords_dict["rows"])
+            c_coords = np.array(coords_dict["cols"])
         
-        elif len(set(coords_dict["rows"])) == 2 and shape_type.find("ellipse") != -1:
+        if shape_type == "rectangle":
             
-            pass
+            mask_dict: dict[str, np.array, tuple] = rectangle_mask(mask_shape, coords_dict, rot_angle)
+        
+        elif shape_type == "ellipse":
             
-        return crop_array
+            mask_dict: dict[str, np.array, tuple] = ellipse_mask(mask_shape, coords_dict, rot_angle)
+            
+        elif shape_type == "polygon":
+            
+            rr, cc = draw.polygon(r_coords, c_coords, shape = mask_shape)
+            mask_array: np.array = np.zeros(mask_shape, dtype = np.bool)
+            mask_array[rr, cc] = 1
+            mask_dict: dict[str, np.array, tuple] = get_mask_dict(mask_array, rr, cc)
+            
+        return mask_dict
     
     else:
         
         print("\nInvalid shape type!")
+
+def crop(im_array: np.array, shape_type: str, shape_coords: np.array, *, outside_mask_int: int | float = 0, conserve_mem: bool = False) -> np.array:
+    
+    valid_shapes: tuple[str] = ("rectangle", "ellipse", "polygon")
+    
+    if any(shape_type.find(x) != -1 for x in valid_shapes):
+        
+        mask_dict: dict[str, np.array, tuple] = coords_2_mask(im_array, shape_coords, shape_type)
+        mask_array = np.expand_dims(mask_dict["mask"], 0)
+        low_coords: tuple = mask_dict["start"]
+        high_coords: tuple = mask_dict["end"]
+        
+        if conserve_mem:
+            
+            if len(im_array.shape) > 2:
+                
+                im_array[np.logical_not(np.repeat(mask_array, im_array.shape[0], axis = 0))] = outside_mask_int
+                
+            else:
+                
+                im_array[np.logical_not(mask_dict["mask"], axis = 0)] = outside_mask_int
+        
+            return im_array[:, low_coords[0]:high_coords[0], low_coords[1]:high_coords[1]]
+        
+        else:
+            
+            crop_array: np.array = np.copy(im_array)
+            
+            if len(im_array.shape) > 2:
+                
+                crop_array[np.logical_not(np.repeat(mask_array, im_array.shape[0], axis = 0))] = outside_mask_int
+                
+            else:
+                
+                crop_array[np.logical_not(mask_dict["mask"], axis = 0)] = outside_mask_int
+        
+            return crop_array[:, low_coords[0]:high_coords[0], low_coords[1]:high_coords[1]]
+    
+    else:
+        
+        print("\nInvalid shape type!")
+        
+def trim_depth(im_array: np.array, bounds: tuple[int]) -> np.array:
+    
+    return im_array[bounds[0]:bounds[1]]
 
 
 # Main
