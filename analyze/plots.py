Fixed second y-axis rotation in plots. Minor bug fixes in quant. Created new util.py file for miscellaneous utility functions. Added percent-based saturation to pixels.

analyze/plots.py
@@ -8,75 +8,134 @@ import numpy as np
 import quant
 
 from matplotlib import pyplot as plt
-from matplotlib import figure
+from matplotlib import figure as fig
+from matplotlib import axes as ax
 
 
 # Functions
 
-def histogram(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False) -> figure.Figure | dict:
+def set_intensity_axlims(axis: ax.Axes, data_type: np.dtype, y_or_x: str = "x", *, axlims: tuple = None) -> ax.Axes:
+    
+    if axlims:
+        
+        if y_or_x == "x":
+            
+            axis.set_xbound(min(axlims), max(axlims))
+        
+        elif y_or_x == "y":
+            
+            axis.set_ybound(min(axlims), max(axlims))
+        
+    else:
+        
+        if data_type == "uint8":
+            
+            if y_or_x == "x":
+                
+                axis.set_xbound(0, 255)
+            
+            elif y_or_x == "y":
+                
+                axis.set_ybound(0, 255)
+        
+        elif data_type == "uint16":
+            
+            if y_or_x == "x":
+                
+                axis.set_xbound(0, 65535)
+            
+            elif y_or_x == "y":
+                
+                axis.set_ybound(0, 65535)
+
+def histogram(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False, axlims: tuple = None) -> fig.Figure | dict:
     
     hist_dict: dict[str, np.ndarray] = quant.get_histogram(im_array, mask_array = mask_array)
-    fig: plt.figure = plt.figure(dpi = 300)
-    fax: plt.axes = plt.axes()
-    fax.figure = fig
+    f: fig.Figure = plt.figure(dpi = 300)
+    fax: ax.Axes = plt.axes()
+    fax.figure = f
     fax.set_xlabel("Intensity")
     fax.set_ylabel("Counts")
     plt.hist(hist_dict["bin centers"], hist_dict["bin centers"], weights = hist_dict["counts"], axes = fax)
     
+    if axlims:
+        
+        fax = set_intensity_axlims(fax, im_array.dtype, "x", axlims = axlims)
+        
+    else:
+        
+        fax.set_xbound(np.min(im_array), np.max(im_array))
+    
     if return_data:
         
-        hist_dict["plot"] = fig
+        hist_dict["plot"] = f
         
         return hist_dict
     
     else:
         
-        return fig
+        return f
 
-def cdf(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False) -> figure.Figure | dict:
+def cdf(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False, axlims: tuple = None) -> fig.Figure | dict:
     
     cdf_dict: dict[str, np.ndarray] = quant.get_cdf(im_array, mask_array = mask_array)
-    fig: plt.figure = plt.figure(dpi = 300)
-    fax: plt.axes = plt.axes()
-    fax.figure = fig
+    f: fig.Figure = plt.figure(dpi = 300)
+    fax: ax.Axes = plt.axes()
+    fax.figure = f
     fax.set_xlabel("Intensity")
     fax.set_ylabel("Probability")
     fax.set_ylim(0, 1)
-    plt.plot(cdf_dict["bin_centers"], cdf_dict["cdf"], "red")
+    plt.plot(cdf_dict["bin centers"], cdf_dict["cdf"], "red")
+    
+    if axlims:
+        
+        fax = set_intensity_axlims(fax, im_array.dtype, "x", axlims = axlims)
+        
+    else:
+        
+        fax.set_xbound(np.min(im_array), np.max(im_array))
     
     if return_data:
         
-        cdf_dict["plot"] = fig
+        cdf_dict["plot"] = f
         
         return cdf_dict
     
     else:
         
-        return fig
+        return f
 
-def hist_and_cdf(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False) -> figure.Figure:
+def hist_and_cdf(im_array: np.ndarray, *, mask_array: np.ndarray = None, return_data: bool = False, axlims: tuple = None) -> fig.Figure | dict:
     
     hist_dict: dict[str, np.ndarray] = quant.get_histogram(im_array, mask_array = mask_array)
     cdf_dict: dict[str, np.ndarray] = quant.get_cdf(im_array, mask_array = mask_array)
-    fig: plt.figure = plt.figure(dpi = 300)
-    fax: plt.axes = plt.axes()
-    fax.figure = fig
+    f: fig.Figure = plt.figure(dpi = 300)
+    fax: ax.Axes = plt.axes()
+    fax.figure = f
     fax.set_xlabel("Intensity")
     fax.set_ylabel("Counts")
     plt.hist(hist_dict["bin centers"], hist_dict["bin centers"], weights = hist_dict["counts"], axes = fax)
     fax2 = fax.twinx()
     fax2.set_xlabel("Intensity")
-    fax2.set_ylabel("Probability")
+    fax2.set_ylabel("Probability", rotation = 270, va = "bottom")
     fax2.set_ylim(0, 1)
-    plt.plot(cdf_dict["bin_centers"], cdf_dict["cdf"], "red")
+    plt.plot(cdf_dict["bin centers"], cdf_dict["cdf"], "red")
+    
+    if axlims:
+        
+        fax = set_intensity_axlims(fax, im_array.dtype, "x", axlims = axlims)
+        
+    else:
+        
+        fax.set_xbound(np.min(im_array), np.max(im_array))
     
     if return_data:
         
-        return {"plot": fig, "histogram": hist_dict, "cdf": cdf_dict}
+        return {"plot": f, "histogram": hist_dict, "cdf": cdf_dict}
     
     else:
         
-        return fig
+        return f
 
 
 # Main

analyze/quant.py
@@ -33,7 +33,7 @@ def get_cdf(im_array: np.ndarray, *, mask_array: np.ndarray = None) -> dict[str,
         
         im_cdf, bin_centers = exposure.cumulative_distribution(im_array)
         
-    return {"cdf": im_cdf, "bin_centers": bin_centers}
+    return {"cdf": im_cdf, "bin centers": bin_centers}
 
 
 # Main

proc/pixels.py
@@ -6,10 +6,12 @@ Module for altering and assessing pixel values and dimensions
 
 import cropclip as cc
 import numpy as np
+import quant
+import util
 
+from skimage import util as skutil
 from skimage import transform
 from skimage import exposure
-from skimage import util
 
 
 # Functions
@@ -36,20 +38,45 @@ def statistics(im_array: np.ndarray, *, mask_array: np.ndarray = None) -> dict:
     
     return im_stats
 
-def get_percentage_intensities(im_array: np.ndarray, percentages: tuple) -> tuple:
+def get_percent_intensities(im_array: np.ndarray, percentages: tuple, *, mask_array: np.ndarray = None, cdf_dict: dict = None) -> tuple:
     
-    pass
+    if max(percentages) > 1:
+        
+        percentages = (percentages[0] / 100, percentages[1] / 100)
+        
+    if not cdf_dict:
+        
+        cdf_dict: dict = quant.get_cdf(im_array, mask_array = mask_array)
+        
+    im_cdf: np.ndarray = cdf_dict["cdf"]
+    bin_centers: np.ndarray = cdf_dict["bin centers"]
+    low_index = util.quick_get_first_index(im_cdf, min(percentages), "greater or equal")
+    high_index = len(im_cdf) - util.quick_get_first_index(np.flip(im_cdf, 0), max(percentages), "less or equal") - 1
+    low_bin = np.astype(bin_centers[low_index], im_array.dtype)
+    high_bin = np.astype(bin_centers[high_index], im_array.dtype)
+    
+    return (low_bin, high_bin)
 
-def saturate(im_array: np.ndarray, bounds: tuple, *, bounds_percent: bool = False) -> np.ndarray:
+def saturate(im_array: np.ndarray, bounds: tuple, *, bounds_percent: bool = True, mask_array: np.ndarray = None, cdf_dict: dict = None, conserve_mem: bool = False) -> np.ndarray:
     
     if bounds_percent:
         
-        pass
+        bounds = get_percent_intensities(im_array, bounds, mask_array = mask_array, cdf_dict = cdf_dict)
         
-    im_array[im_array > max(bounds)] = max(bounds)
-    im_array[im_array < min(bounds)] = min(bounds)
+    if conserve_mem:
+        
+        im_array[im_array > max(bounds)] = max(bounds)
+        im_array[im_array < min(bounds)] = min(bounds)
     
-    return im_array
+        return im_array
+    
+    else:
+        
+        sat_array = np.copy(im_array)
+        sat_array[im_array > max(bounds)] = max(bounds)
+        sat_array[im_array < min(bounds)] = min(bounds)
+        
+        return sat_array
 
 def normalize(im_array: np.ndarray, norm_bounds: tuple) -> np.ndarray:
     
@@ -71,31 +98,31 @@ def convert_im_type(im_array: np.ndarray, convert_type: str, *, norm: bool = Fal
             
         if convert_type == "uint8":
                     
-            conv_array: np.ndarray = util.img_as_ubyte(im_array)
+            conv_array: np.ndarray = skutil.img_as_ubyte(im_array)
                 
         elif convert_type == "uint16":
                     
-            conv_array: np.ndarray = util.img_as_uint(im_array)
+            conv_array: np.ndarray = skutil.img_as_uint(im_array)
                 
         elif convert_type == "int16":
                     
-            conv_array: np.ndarray = util.img_as_int(im_array)
+            conv_array: np.ndarray = skutil.img_as_int(im_array)
                 
         elif convert_type == "float":
                     
-            conv_array: np.ndarray = util.img_as_float(im_array)
+            conv_array: np.ndarray = skutil.img_as_float(im_array)
                     
         elif convert_type == "float32":
                     
-            conv_array: np.ndarray = util.img_as_float32(im_array)
+            conv_array: np.ndarray = skutil.img_as_float32(im_array)
                     
         elif convert_type == "float64":
                     
-            conv_array: np.ndarray = util.img_as_float64(im_array)
+            conv_array: np.ndarray = skutil.img_as_float64(im_array)
                 
         elif convert_type == "bool":
                     
-            conv_array: np.ndarray = util.img_as_bool(im_array)
+            conv_array: np.ndarray = skutil.img_as_bool(im_array)
             
         return conv_array
         
@@ -123,7 +150,7 @@ def rescale(im_array: np.ndarray, scale: float) -> np.ndarray:
         
 def invert(im_array: np.ndarray) -> np.ndarray:
     
-    return util.invert(im_array)
+    return skutil.invert(im_array)
 
 def histogram_equalization(im_array: np.ndarray, method: str = "global", *, mask_array: np.ndarray = None, kernel_size = None, clip_limit = 0.01) -> np.ndarray:
     